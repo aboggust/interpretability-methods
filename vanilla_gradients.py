718426ff95dc166ab5390d5c9b5ade5e378e4077
Angie Boggust <aboggust@mit.edu>
2021-07-15 16:05:01 -0400

    Captum import update.

--- vanilla_gradients.py
+++ vanilla_gradients.py
@@ -1,7 +1,7 @@
 # Vanilla gradients interpretability method class. 
 # Original papers: https://www.researchgate.net/profile/Aaron_Courville/publication/265022827_Visualizing_Higher-Layer_Features_of_a_Deep_Network/links/53ff82b00cf24c81027da530.pdf; https://arxiv.org/pdf/1312.6034.pdf
 
-import captum
+import captum.attr
 import torch
 import numpy as np
 
