affaaf1d953858392c71425d7a5f1eb818b706a9
Angie Boggust <aboggust@mit.edu>
2021-12-24 12:17:22 -0500

    Updated binarize mask.

--- util.py
+++ util.py
@@ -56,17 +56,17 @@ def binarize_masks(batch, num_std=1):
     Creates binary mask by thresholding at one standard deviation above the mean.
 
     Args:
-    batch: 4D numpy array (batch, channels, height, width).
+    batch: 3D numpy array (batch, height, width).
     threshold: float in range 0 to 1. Values above the threshold are set to 1.
         Values below the threshold are set to 0.
 
     Returns:
-    A 4D numpy array with dtype uint8 with all values set to 0 or 1.
+    A 3D numpy array with dtype uint8 with all values set to 0 or 1.
     """
     batch_size = batch.shape[0]
     batch_normalized = normalize_0to1(batch)
-    mean = np.mean(batch_normalized, axis=(1, 2, 3)).reshape(batch_size, 1, 1, 1)
-    std = np.std(batch_normalized, axis=(1, 2, 3)).reshape(batch_size, 1, 1, 1)
+    mean = np.mean(batch_normalized, axis=(1, 2)).reshape(batch_size, 1, 1)
+    std = np.std(batch_normalized, axis=(1, 2)).reshape(batch_size, 1, 1)
     threshold = mean + num_std * std
     binary_mask = (batch_normalized >= threshold).astype('uint8')
     return binary_mask
