3f2f43a9f2a8b293ae05c90f3623c9d6ec609e62
Angie Boggust <aboggust@mit.edu>
2022-02-08 17:16:28 -0500

    Reorganized from easy install.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='interpretability_methods',
+      version='0.1',
+      description='Interpretability methods for deep learning.',
+      url='https://github.com/aboggust/interpretability-methods',
+      author='Angie Boggust',
+      author_email='aboggust@mit.edu',
+      license='MIT',
+      packages=['interpretability_methods'],
+      zip_safe=False)
